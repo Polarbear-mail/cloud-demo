9280e05603d78f9f924d9ba354cc6d3bbb97950c
Polarbear <polarbear-mail@qq.com>
2022-12-03 22:35:20 +0800

    nacos统一配置管理 commit

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ：Polarbear
+ * @date ：Created 2022/12/1 21:55
+ * @description：
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
